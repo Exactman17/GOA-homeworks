8a941f07e431959ec436875b9274ca5460000c8f
Exactman17 <ggelashvili334@gmail.com>
2024-01-27 23:47:20 +0400

    first commit

---
+++ Day 1/lesson1.py
@@ -0,0 +1,115 @@
+from turtle import *
+# vashenebt saxls
+speed(15)
+
+#step 1  sadzirkveli
+width(7)
+color("blue")
+
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+# kvadrati dasrulebulia
+
+#vaketebt karebs
+
+
+forward(70)
+width(10)
+color("green")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+#karebi dasrulebulia jigaro
+penup()
+#saxuravi
+
+goto(200,200)
+pendown()
+
+width(7)
+color("red")
+
+begin_fill()
+
+
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+# wvimisgan daculebi vart
+
+#gavixedot garetac
+
+penup()
+goto(20,150)
+
+pendown()
+
+width(5)
+color("brown")
+
+
+right(150)
+forward(30)
+right(90)
+forward(60)
+right(90)
+forward(30)
+right(90)
+forward(60)
+
+
+penup()
+goto(180,150)   
+pendown()
+
+forward(60)
+right(90)   
+forward(30) 
+right(90)
+forward(60)
+right(90)
+forward(30)
+
+#shemobrdzandit door is open
+
+# karebi igeba shignit
+
+penup()
+
+goto(110,50)
+pendown()
+width(3)
+color("black")
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+
+#karis gasagebat ruchka chamowiet dabla
+
+penup()
+goto(0,0)
+pendown()
+
+#finita la komedia
+
+
+
+exitonclick()
