1f5ae3fa23959f6f46bba77078d40459d7137ffd
Exactman17 <157917744+Exactman17@users.noreply.github.com>
2024-02-08 23:49:44 +0400

    Delete Day 2 directory

--- Day 2/day 2.py
+++
@@ -1,7 +0,0 @@
-
-
-
-print("გავიცანი და დავუმეგობრდი: barbares romelic chemi razmis lideria da damexmara ragaceebis garkvevashi " ) 
-
-
-print("რას ვფიქრობ GOAზე : Goa namdvilad momavlis adamianebis akvania, mixaria rom miuxedavad chemi asakisa meqneba shansi im xalxtan ertad sheviswavlo es tema vinc momavals sheqmnis" ) 
